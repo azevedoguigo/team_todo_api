e1d8ae3b05807f004066b2992e00155b48b11d9a
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-06-19 01:11:53 -0300

    chore: users schema and changeset.

---
+++ lib/team_todo_api/accounts/user.ex
@@ -0,0 +1,38 @@
+defmodule TeamTodoApi.Accounts.User do
+  @moduledoc """
+  This module provides the schema and user changeset.
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+  @required_fields [:name, :email, :password]
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash
+
+    timestamps()
+  end
+
+  def chageset(user, params) do
+    user
+    |> cast(params, @required_fields)
+    |> validate_required(@required_fields)
+    |> validate_length(:name, min: 2, max: 40)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:email, min: 11, max: 40)
+    |> unique_constraint(:email, name: :users_email_index)
+    |> validate_length(:password, min: 6, max: 30)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = chageset) do
+    change(chageset, Argon2.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
