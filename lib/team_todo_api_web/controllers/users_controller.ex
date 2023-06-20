fe1f19dcb072c50c504bbf182aab75e328b30454
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-06-20 18:28:48 -0300

    refactor: using the humanize_errors/1 function and returning the created user.

--- lib/team_todo_api_web/controllers/users_controller.ex
+++ lib/team_todo_api_web/controllers/users_controller.ex
@@ -1,19 +1,20 @@
 defmodule TeamTodoApiWeb.UsersController do
   use TeamTodoApiWeb, :controller
 
+  alias TeamTodoApiWeb.ErrorHandler
   alias TeamTodoApi.Users.Create
 
   def create(conn, params) do
     case Create.create_user(params) do
-      {:ok, _user} ->
+      {:ok, user} ->
         conn
         |> put_status(:created)
-        |> json(%{message: "User created!"})
+        |> json(%{message: "User created!", user: user})
 
-      {:error, _changeset} ->
+      {:error, changeset} ->
         conn
         |> put_status(:bad_request)
-        |> json(%{message: "Error to create user!"})
+        |> json(%{error: ErrorHandler.humanize_errors(changeset)})
     end
   end
 end
