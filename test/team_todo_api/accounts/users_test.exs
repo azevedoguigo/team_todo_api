120c885e60cb5910a0fbc7456a9c32b2c56c2762
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-06-20 06:38:04 -0300

    test: create/1 tests created.

---
+++ test/team_todo_api/accounts/users_test.exs
@@ -0,0 +1,45 @@
+defmodule TeamTodoApi.UsersTest do
+  use TeamTodoApi.DataCase
+
+  alias TeamTodoApi.{Schemas, Accounts}
+  alias TeamTodoApi.Repo
+
+  describe "create/1" do
+    test "When all parameters are valid, it registers the user." do
+      params = %{
+        name: "azevedoguigo",
+        email: "azevedoguigo.test@example.com",
+        password: "supersenha"
+      }
+
+      count_before = Repo.aggregate(Schemas.User, :count)
+
+      response = Accounts.Users.create(params)
+
+      count_after = Repo.aggregate(Schemas.User, :count)
+
+      assert {
+        :ok,
+        %Schemas.User{
+          name: "azevedoguigo",
+          email: "azevedoguigo.test@example.com",
+          password: "supersenha"
+        }
+      } = response
+
+      assert count_after > count_before
+    end
+
+    test "When any of the parameters are valid, it returns an error message." do
+      params = %{
+        name: "",
+        email: "azevedoguigo.testexample.com", # Invalid email format.
+        password: "supersenha"
+      }
+
+      {:error, changeset} = Accounts.Users.create(params)
+
+      assert errors_on(changeset) == %{name: ["can't be blank"], email: ["has invalid format"]}
+    end
+  end
+end
