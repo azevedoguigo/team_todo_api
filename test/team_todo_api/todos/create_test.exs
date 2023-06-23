51c768e22ed9b15c09af2e2882d924d4a09c357a
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-06-23 08:26:00 -0300

    test: create_todo/2 tests.

---
+++ test/team_todo_api/todos/create_test.exs
@@ -0,0 +1,40 @@
+defmodule TeamTodoApi.Todos.CreateTest do
+  use TeamTodoApi.DataCase
+
+  alias TeamTodoApi.Schemas.{User, Todo}
+  alias TeamTodoApi.{Users, Todos}
+
+  @user_default_params %{
+    name: "azevedoguigo",
+    email: "test@gmail.com",
+    password: "supersenha"
+  }
+
+  describe "create_todo/2" do
+    test "When the parameters are valid, returns the new todo." do
+      {:ok, %User{id: user_id}} = Users.Create.create_user(@user_default_params)
+
+      params = %{
+        "title" => "My ToDo title",
+        "description" => "My ToDo description."
+      }
+
+      result = Todos.Create.create_todo(params, user_id)
+
+      assert{:ok, %Todo{title: "My ToDo title", description: "My ToDo description."}} = result
+    end
+
+    test "When one or more parameters are invalid, returns an invalid changeset." do
+      {:ok, %User{id: user_id}} = Users.Create.create_user(@user_default_params)
+
+      params = %{
+        "title" => "", # The title cannot be blank.
+        "description" => "My ToDo description."
+      }
+
+      {:error, invalid_changeset} = Todos.Create.create_todo(params, user_id)
+
+      assert errors_on(invalid_changeset) == %{title: ["can't be blank"]}
+    end
+  end
+end
