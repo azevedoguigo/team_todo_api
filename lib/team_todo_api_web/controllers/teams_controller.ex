2dfb90f543c563d4498b6e96bf69e0e139096d82
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-09-06 22:20:27 -0300

    refactor: using fallback controller.

--- lib/team_todo_api_web/controllers/teams_controller.ex
+++ lib/team_todo_api_web/controllers/teams_controller.ex
@@ -1,22 +1,18 @@
 defmodule TeamTodoApiWeb.TeamsController do
   use TeamTodoApiWeb, :controller
 
-  alias TeamTodoApi.{Teams}
+  alias TeamTodoApi.Teams.Create
   alias TeamTodoApi.Schemas.User
+
+  action_fallback TeamTodoApiWeb.FallbackController
 
   def create(conn, params) do
     %User{id: user_id} = Guardian.Plug.current_resource(conn)
 
-    case Teams.Create.create_team(params, user_id) do
-      {:ok, team} ->
-        conn
-        |> put_status(:created)
-        |> json(%{message: "Team created!", team: team})
-
-      {:error, message} ->
-        conn
-        |> put_status(:bad_request)
-        |> json(%{error: message})
+    with {:ok, team} <- Create.create_team(params, user_id) do
+      conn
+      |> put_status(:created)
+      |> json(%{message: "Team created!", team: team})
     end
   end
 end
