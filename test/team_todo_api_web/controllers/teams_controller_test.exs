97e06b0ef3c982b690c2cf9f89a42b87ce490a3b
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-09-13 19:03:26 -0300

    test: creted create team endpoint tests.

---
+++ test/team_todo_api_web/controllers/teams_controller_test.exs
@@ -0,0 +1,62 @@
+defmodule TeamTodoApiWeb.TeamsControllerTest do
+  use TeamTodoApiWeb.ConnCase
+
+  alias TeamTodoApiWeb.Auth.Guardian
+  alias TeamTodoApi.Schemas.User
+  alias TeamTodoApi.Users
+
+  @team_default_params %{
+    name: "My team",
+    description: "My test team"
+  }
+
+  setup %{conn: conn} do
+    user_create_params = %{
+      name: "azevedoguigo",
+      email: "test@gmail.com",
+      password: "supersenha"
+    }
+
+    {:ok, %User{id: user_id} = user} = Users.Create.create_user(user_create_params)
+    {:ok, token, _claims} = Guardian.encode_and_sign(user)
+
+    conn = put_req_header(conn, "authorization", "Bearer #{token}")
+
+    {:ok, conn: conn, user_id: user_id}
+  end
+
+  describe "create/2" do
+    test "Creates and returns a team when settings are valid.", %{conn: conn, user_id: user_id} do
+      response =
+        conn
+        |> post(~p"/api/teams", Map.put(@team_default_params, :user_id, user_id))
+        |> json_response(:created)
+
+      expected_response = %{
+        "message" => "Team created!",
+        "team" => %{
+          "name" => "My team",
+          "description" => "My test team",
+          "user_id" => user_id
+        }
+      }
+
+      assert expected_response = response
+    end
+
+    test "When any mandatory parameter is invalid, an error is returned.", %{conn: conn, user_id: user_id} do
+      with_invalid_param = %{
+        title: "", # Blank title!
+        description: "My test team",
+        user_id: user_id
+      }
+
+      response =
+        conn
+        |> post(~p"/api/teams", with_invalid_param)
+        |> json_response(:bad_request)
+
+      assert %{"errors" => %{"name" => ["can't be blank"]}} == response
+    end
+  end
+end
