1cd7377652d4f393537063b8717ca12c02d2d7b6
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-09-13 22:28:08 -0300

    test: created get team action tests.

--- test/team_todo_api_web/controllers/teams_controller_test.exs
+++ test/team_todo_api_web/controllers/teams_controller_test.exs
@@ -2,8 +2,8 @@ defmodule TeamTodoApiWeb.TeamsControllerTest do
   use TeamTodoApiWeb.ConnCase
 
   alias TeamTodoApiWeb.Auth.Guardian
-  alias TeamTodoApi.Schemas.User
-  alias TeamTodoApi.Users
+  alias TeamTodoApi.Schemas.{User, Team}
+  alias TeamTodoApi.{Users, Teams}
 
   @team_default_params %{
     name: "My team",
@@ -56,6 +56,46 @@ defmodule TeamTodoApiWeb.TeamsControllerTest do
         |> json_response(:bad_request)
 
       assert %{"errors" => %{"name" => ["can't be blank"]}} == response
+    end
+  end
+
+  describe "get/2" do
+    test "Returns the team as long as the id is valid and corresponds to a team.", %{conn: conn, user_id: user_id} do
+      create_team_params = %{
+        "name" => "My Team",
+        "description" => "Testing team creation."
+      }
+
+      {:ok, %Team{id: team_id}} = Teams.Create.create_team(create_team_params, user_id)
+
+      response =
+        conn
+        |> get(~p"/api/teams?team_id=#{team_id}")
+        |> json_response(:ok)
+
+        assert %{
+          "name" => "My Team",
+          "description" => "Testing team creation.",
+          "user_id" => ^user_id
+        } = response
+    end
+
+    test "Returns an error message when the team is not found.", %{conn: conn} do
+      response =
+        conn
+        |> get(~p"/api/teams?team_id=fe9d6a40-631c-4e19-84f4-197ef55deb0e")
+        |> json_response(:not_found)
+
+        assert %{"error" => "This team does not exist!"} == response
+    end
+
+    test "Returns an error message when the team is invalid.", %{conn: conn} do
+      response =
+        conn
+        |> get(~p"/api/teams?team_id=invalid_id")
+        |> json_response(:bad_request)
+
+        assert %{"error" => "Invalid team ID!"} == response
     end
   end
 end
